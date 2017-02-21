a9098ea0503a2eb65a79dfddca3ded7c9b140fa8
Jean Privat <jean@pryen.org>
2017-02-21 10:55:07 -0500

    Fix small typo in doc in utils.py s/A-z/A-Z/

--- lib/utils.py
+++ lib/utils.py
@@ -564,7 +564,7 @@ def cyclic_pattern_charset(charset_type=None):
 
     Args:
         - charset_type: charset type
-            0: basic (0-9A-za-z)
+            0: basic (0-9A-Za-z)
             1: extended (default)
             2: maximum (almost printable chars)
 
@@ -635,7 +635,7 @@ def cyclic_pattern(size=None, start=None, charset_type=None):
         - size: size of generated pattern (Int)
         - start: the start offset of the generated pattern (Int)
         - charset_type: charset type
-            0: basic (0-9A-za-z)
+            0: basic (0-9A-Za-z)
             1: extended (default)
             2: maximum (almost printable chars)
 
