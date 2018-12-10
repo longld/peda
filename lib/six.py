34e48265f68c03f54e5b230cb365e37069a4b329
cclauss <cclauss@bluewin.ch>
2018-12-10 07:03:09 +0100

    Upgrade to six 1.12.0
    https://github.com/benjaminp/six/blob/master/CHANGES

--- lib/six.py
+++ lib/six.py
@@ -29,7 +29,7 @@ import sys
 import types
 
 __author__ = "Benjamin Peterson <benjamin@python.org>"
-__version__ = "1.11.0"
+__version__ = "1.12.0"
 
 
 # Useful for very coarse version differentiation.
@@ -844,6 +844,8 @@ def add_metaclass(metaclass):
                 orig_vars.pop(slots_var)
         orig_vars.pop('__dict__', None)
         orig_vars.pop('__weakref__', None)
+        if hasattr(cls, '__qualname__'):
+            orig_vars['__qualname__'] = cls.__qualname__
         return metaclass(cls.__name__, cls.__bases__, orig_vars)
     return wrapper
 
